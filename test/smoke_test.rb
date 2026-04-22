1ea0e192218d1262532cd7ad9628367758d1dc79
victor <victor@elalemanyo.de>
2026-04-22 23:13:07 +0200

    fix: Correct require_relative path in smoke_test.rb

--- test/smoke_test.rb
+++ test/smoke_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "minitest/autorun"
-require_relative "./test_helper"
+require_relative "test_helper"
 
 class SmokeTest < Minitest::Test
   def test_module_loads
